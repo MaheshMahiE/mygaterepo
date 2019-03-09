156a596d0493b554604cbf3adeeef5ace9d463e1
mahesh <emahesh16@gmail.com>
2019-03-10 02:16:06 +0530

    initializing i value

--- mygate-git/src/main/java/com/mygate/gitdemo/MyGateRepo.java
+++ mygate-git/src/main/java/com/mygate/gitdemo/MyGateRepo.java
@@ -2,6 +2,7 @@ package com.mygate.gitdemo;
 
 public class MyGateRepo{
 	public static void main(String args[]) {
+		int i=0;
 		System.out.println("this is MyGateRepo class 5th line in merchant-system branch");
 	}
 }

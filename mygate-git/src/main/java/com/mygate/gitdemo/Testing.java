65e64eedeb40ddab679d99258b1377baca322a47
mahesh <emahesh16@gmail.com>
2019-03-09 23:32:04 +0530

    small changes

--- mygate-git/src/main/java/com/mygate/gitdemo/Testing.java
+++ mygate-git/src/main/java/com/mygate/gitdemo/Testing.java
@@ -2,6 +2,6 @@ package com.mygate.gitdemo;
 
 public class Testing {
 	public static void main(String[] args) {
-		System.out.println("this is Testing class line 5");
+		System.out.println("this is Testing class line 5 in master branch");
 	}
 }

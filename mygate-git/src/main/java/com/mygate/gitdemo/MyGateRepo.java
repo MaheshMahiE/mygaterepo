93da680ea0fcba7aa8d0f70bcd70a7d7ce1e6c00
mahesh <emahesh16@gmail.com>
2019-03-09 23:26:03 +0530

    small changes

--- mygate-git/src/main/java/com/mygate/gitdemo/MyGateRepo.java
+++ mygate-git/src/main/java/com/mygate/gitdemo/MyGateRepo.java
@@ -1,5 +1,7 @@
 package com.mygate.gitdemo;
 
 public class MyGateRepo{
-
+	public static void main(String args[]) {
+		System.out.println("this is 5th line");
+	}
 }
--- mygate-git/src/main/java/com/mygate/gitdemo/MyGateRepoGit.java
+++ mygate-git/src/main/java/com/mygate/gitdemo/MyGateRepoGit.java
@@ -1,5 +1,7 @@
 package com.mygate.gitdemo;
 
 public class MyGateRepoGit{
-
+	public static void main(String args[]) {
+		System.out.println("this is MyGateRepoGit class 5th line");
+	}
 }

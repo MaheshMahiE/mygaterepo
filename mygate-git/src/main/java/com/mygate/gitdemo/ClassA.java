581a634aee28733995694bb30fc70d9da644e6d5
mahesh <emahesh16@gmail.com>
2019-03-16 00:56:40 +0530

    new changes

--- mygate-git/src/main/java/com/mygate/gitdemo/ClassA.java
+++ mygate-git/src/main/java/com/mygate/gitdemo/ClassA.java
@@ -3,5 +3,6 @@ package com.mygate.gitdemo;
 public class ClassA {
 	public static void main(String[] args) {
 		System.out.println("this is ClassA updated through developer branch");
+		System.out.println("this is ClassA updated through developer branch 2nd time");
 	}
 }
